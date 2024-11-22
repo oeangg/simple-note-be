03754ff5fa5e6c73386c284eb9057f31a29f0df3
Your Nameoeangg <msubhan33@gmail.com>
2024-11-22 11:22:03 +0700

    add:route get,get single,post,patch,delete & swagger

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,96 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { prisma } from "./utils/prisma";
+import { swagger } from "@elysiajs/swagger";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+  .use(
+    swagger({
+      path: "/docs",
+    })
+  )
+  //route Get All note
+  .get("/", async () => {
+    const notes = await prisma.note.findMany();
+
+    return notes;
+  })
+
+  //route Get Single note
+  .get("/:id", async ({ params }) => {
+    const { id } = params;
+    const findNote = await prisma.note.findFirst({
+      where: {
+        id,
+      },
+    });
+
+    return findNote;
+  })
+
+  //route post note
+  .post(
+    "/",
+    async ({ body }) => {
+      const { content } = body;
+
+      const newNote = await prisma.note.create({
+        data: {
+          content,
+        },
+      });
+
+      return newNote;
+    },
+
+    //schema guard content body
+    {
+      body: t.Object({
+        content: t.String(),
+      }),
+    }
+  )
+
+  //route edit Note
+  .patch(
+    "/:id",
+    async ({ params, body }) => {
+      const { id } = params;
+      const { content, isDone } = body;
+
+      const updateNote = await prisma.note.update({
+        where: {
+          id,
+        },
+        data: {
+          content,
+          isDone,
+        },
+      });
+
+      return updateNote;
+    },
+    {
+      body: t.Object({
+        content: t.String(),
+        isDone: t.Boolean(),
+      }),
+    }
+  )
+
+  //route delete note
+  .delete("/:id", async ({ params }) => {
+    const { id } = params;
+
+    await prisma.note.delete({
+      where: {
+        id,
+      },
+    });
+
+    return "deleted succses";
+  })
+
+  .listen(3000);
+
+console.log("Note BE App");
